dd24eb978d75dfd0633cea630cd9e3fd4fbcaba8
Ryan Daley <rdaley321@yahoo.com>
2018-12-11 02:00:50 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|e| e.name == item_name}
+      if @@items.include?(item)
+       resp.write(item.price)
+      else
+        resp.status = 400
+        resp.write("Item not found")
+      end
+    else
+      resp.status = 404
+      resp.write("Route not found")
+    end
+    
+    resp.finish
+    
+  end
+end
