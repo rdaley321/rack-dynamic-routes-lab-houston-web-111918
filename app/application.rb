44054051adcf1a5af8483ced8a66126f4ea1072b
Ryan Daley <rdaley321@yahoo.com>
2018-12-11 01:50:39 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item = req.path.split(/items/).last
+    else
+      resp.status("404")
+    end
+    
+    
+    
+  end
+end
